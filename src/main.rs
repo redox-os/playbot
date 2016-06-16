217040939365261fd57e3066cca22521eaf00e1b
ticki <ticki@users.noreply.github.com>
2016-06-16 23:46:41 +0200

    Skip preprocessing when using `fn main`

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,46 @@ extern crate mars;
 extern crate playpen;
 
 use mars::{Bot, Response};
+use std::borrow::Cow;
 use std::error::Error;
 
-fn process(msg: &str) -> String {
-    String::from("fn main() { println!(\"{:?}\", {\n")
-        + msg.trim_left_matches("playbot:")
-             .trim()
-             .trim_left_matches("```rust")
-             .trim_matches('`')
-        + "\n}); }"
+fn normalize(mut msg: &str) -> &str {
+    if msg.starts_with("playbot") {
+        msg = &msg[7..];
+
+        if msg.starts_with(':') {
+            msg = &msg[1..];
+        }
+    }
+
+    msg = msg.trim();
+
+    if msg.starts_with("```") {
+        msg = &msg[3..];
+
+        if msg.starts_with("rust\n") {
+            msg = &msg[4..];
+        }
+    } else if msg.starts_with('`') {
+        msg = &msg[1..];
+    }
+
+    msg.trim_right_matches('`').trim()
+}
+
+fn process(msg: &str) -> Cow<str> {
+    let norm = normalize(msg);
+
+    if norm.contains("fn main") {
+        norm.into()
+    } else {
+        format!("fn main() {{ println!(\"{{:?}}\", {{\n{}\n}}); }}", norm).into()
+    }
 }
 
 fn main() {
     Bot::new(env!("TOKEN"), |req| {
-        let res = playpen::eval(process(&req.text).as_str());
+        let res = playpen::eval(&*process(&req.text));
 
         Response {
             username: Some("playbot".into()),
@@ -28,4 +54,28 @@ fn main() {
             icon_url: Some("http://www.rustacean.net/assets/rustacean-orig-trans.png".into()),
         }
     }).init(env!("IP")).unwrap();
+}
+
+#[cfg(test)]
+#[test]
+fn test_normalize() {
+    assert_eq!(normalize("playbot blah"), "blah");
+    assert_eq!(normalize("playbot: blah"), "blah");
+    assert_eq!(normalize("playbot: ```\nabc\n```"), "abc");
+    assert_eq!(normalize("playbot: ```rust\n\
+                         abc\n\
+                         ```"), "abc");
+    assert_eq!(normalize("playbot: `lulz`"), "lulz");
+    assert_eq!(normalize("playbot `lulz`"), "lulz");
+    assert_eq!(normalize("playbot :lal"), ":lal");
+}
+
+#[cfg(test)]
+#[test]
+fn test_process() {
+    assert_eq!(process("use std::io; fn main() { /* blah */ }"), "use std::io; fn main() { /* blah */ }");
+    assert_eq!(process("playbot use std::io; fn main() { /* blah */ }"), "use std::io; fn main() { /* blah */ }");
+    assert_eq!(process("playbot: use std::io; fn main() { /* blah */ }"), "use std::io; fn main() { /* blah */ }");
+    assert_eq!(process("playbot: `use std::io; fn main() { /* blah */ }`"), "use std::io; fn main() { /* blah */ }");
+    assert_eq!(process("playbot: `use std::io;`"), "fn main() { println!(\"{:?}\", {\nuse std::io;\n}); }");
 }
